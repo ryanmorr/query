ff6ec8b897fed3147c399c803c9037769dd5cc89
Ryan Morr <rm.morr@gmail.com>
2015-01-30 22:03:42 -0500

    refactored

--- query.js
+++ query.js
@@ -1,28 +1,37 @@
 /*
  * query
- * Abstraction to querySelectorAll for increased performance and greater usability
+ * Abstraction to querySelectorAll for increased 
+ * performance and greater usability
  * @param {String} selector
- * @param {Element} context
- * @return {Element/Array}
+ * @param {Element} context (optional)
+ * @return {Array}
  */
 
 (function(win){
     'use strict';
 
-    var doc = win.document, simpleRe = /^(#?[\w-]+|\.[\w-.]+)$/, periodRe = /\./g, slice = [].slice;
+    var doc = win.document, 
+    simpleRe = /^(#?[\w-]+|\.[\w-.]+)$/, 
+    periodRe = /\./g, 
+    slice = [].slice;
 
     win.query = function(selector, context){
         context = context || doc;
+        // Redirect call to the more performant function 
+        // if it's a simple selector and return an array
+        // for easier usage
         if(simpleRe.test(selector)){
-            switch(selector.charAt(0)){
+            switch(selector[0]){
                 case '#':
-                    return context.getElementById(selector.substr(1));
+                    return [context.getElementById(selector.substr(1))];
                 case '.':
                     return slice.call(context.getElementsByClassName(selector.substr(1).replace(periodRe, ' ')));
                 default:
                     return slice.call(context.getElementsByTagName(selector));
             }
         }
+        // If not a simple selector, query the DOM as usual 
+        // and return an array for easier usage
         return slice.call(context.querySelectorAll(selector));
     };
     
